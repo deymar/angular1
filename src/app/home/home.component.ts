e2f701f50eb814238c1b4d1e244f798389f25202
deymar <deytorrez7software@gmail.com>
2019-07-09 18:40:27 -0400

    avance de ejercicio de servicio

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,11 +10,16 @@ export class HomeComponent implements OnInit {
 
 public titulo = " TAG de pagina principal";
 public servicio_dato:string;
+/// array apra capturar los datos de servicio
+public listado_ropa:Array<string>;
+// variable para prenda a guardar
+public prenda_guardar:string;
+
+
+
   constructor(   public _unoservice: UnoService  ) 
   { 
-    ///instanciando el servicio
-    
-    /////
+
 
   }
 
@@ -24,6 +29,25 @@ public servicio_dato:string;
     console.log(this._unoservice.prueba());
     ////
     this.servicio_dato=this._unoservice.prueba();
+
+    this.listado_ropa=this._unoservice.getRopa();
+   console.log(this.listado_ropa);
+
+   }
+
+   guardarPrenda()
+   {
+     var parametro:string =this.prenda_guardar;
+     this._unoservice.addRopa(parametro);
+     this.prenda_guardar=null;
+   }
+
+   eliminarPrenda(indice:number)
+   {
+
+     this._unoservice.deleteRopa(indice);
+      alert(indice);
+
    }
 
 }
--- src/app/uno.service.ts
+++ src/app/uno.service.ts
@@ -5,24 +5,39 @@ import { Injectable } from '@angular/core';
 })
 export class UnoService {
   public nombre_prenda ="Pantalones desde el servicio";
-  public colleccion_ropa =["Pantalones desde el servicio","camiseta"];
-  
- 
+  public colleccion_ropa =["Polera","camiseta"];
+
+  constructor()
+  {}
+
   prueba()
   {
      return this.nombre_prenda;
   }
- 
- 
-  constructor() 
-  { 
-    
-  }
 
-  addRopa(nombre_prenda)
-  {
-     return this.colleccion_ropa.push(this.nombre_prenda);
-  }
+
+ addRopa(nombre_prenda:string):Array<string>
+ {
+   this.colleccion_ropa.push(nombre_prenda);
+   return this.getRopa();
+ }
+
+ getRopa():Array<string>
+ {
+    return this.colleccion_ropa;
+ }
+
+ deleteRopa(indice:number)
+ {
+this.colleccion_ropa.splice(indice,1);
+
+return this.getRopa();
+
+ }
+ 
+
+
+
 
   
 }
